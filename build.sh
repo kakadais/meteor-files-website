5f66723732b167305dac1c648d92f880ce04c411
Ubuntu <ubuntu@ip-172-31-2-209.ap-northeast-2.compute.internal>
2023-10-31 20:06:28 +0900

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-rm -rf .meteor/build/*
-meteor build .meteor/build
-cd .meteor/build
+rm -rf .meteor/local/___build/*
+meteor build .meteor/local/___build
+cd .meteor/local/___build
 tar zxvf *.tar.gz
 cd bundle/programs/server
 meteor npm install --production
